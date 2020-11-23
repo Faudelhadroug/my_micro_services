7c27ed95d9e7dafc0ec0087350feacee4e0685a6
Faudelhadroug <faudel.hadroug@epitech.eu>
2020-11-23 11:58:28 +0100

    adding connection mongodb and server run

---
+++ nodejs_service/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3100;
+
+let dev_db_url = 'mongodb+srv://root:root@test.6q70q.mongodb.net/micro_service?retryWrites=true&w=majority';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(
+    mongoDB,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+});
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on ('error', console.error.bind(console,'Connexion error on MongoDB : '));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(port, () => console.log(`server running on port: ${port}`))
